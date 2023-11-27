92ddb461969b87b86cdcadd809d4a8579f1e3b3a
Phan Quoc Dinh <dinh2631984@gmail.com>
2023-11-28 01:03:56 +0700

    28-11-2023 01

--- CIT/assets/js.js
+++ CIT/assets/js.js
@@ -1,3 +1,4 @@
+/*
 const a = document.querySelector('.nft_coverProfile');
 
 function myFunction(x) {
@@ -6,7 +7,20 @@ function myFunction(x) {
 
     }
 }
+*/
 
-var x = window.matchMedia("(max-width: 834px)")
+/*var x = window.matchMedia("(max-width: 834px)")
 myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+x.addListener(myFunction) // Attach listener function on state changes*/
+
+const modal = document.querySelector('.modal');
+const nftNavReponsive = document.querySelector('.nft_header .nft_nav-reponsive');
+const nftNavRes = document.querySelector('.nft_nav-res');
+
+console.log(nftNavReponsive);
+console.log(modal)
+console.log(nftNavRes)
+nftNavReponsive.addEventListener('click', () => {
+   modal.classList.toggle('isShow');
+   nftNavRes.classList.add('isShowRes');
+});
