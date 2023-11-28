9424f696c7c1a6a4a9818ec5931aa2a9236e2acc
dinh2631984 <dinh2631984@gmail.com>
2023-11-28 23:06:07 +0700

    28-11-2023 01

--- CIT/assets/js.js
+++ CIT/assets/js.js
@@ -17,10 +17,51 @@ const modal = document.querySelector('.modal');
 const nftNavReponsive = document.querySelector('.nft_header .nft_nav-reponsive');
 const nftNavRes = document.querySelector('.nft_nav-res');
 
-console.log(nftNavReponsive);
+/*console.log(nftNavReponsive);
 console.log(modal)
-console.log(nftNavRes)
+console.log(nftNavRes)*/
 nftNavReponsive.addEventListener('click', () => {
-   modal.classList.toggle('isShow');
-   nftNavRes.classList.add('isShowRes');
+    modal.classList.toggle('isShow');
+    // nftNavRes.classList.add('isShowRes');
+});
+
+modal.addEventListener('click', () => {
+    modal.classList.toggle('isShow');
+})
+
+nftNavRes.addEventListener('click', (e) => e.stopImmediatePropagation());
+
+
+/*  For tabBar    */
+const nfttabBarList = document.querySelectorAll('.nft_tabBar-item');
+// const nfttabBarAfter = document.querySelectorAll('.nft_tabBar-item::after');
+// console.log(nfttabBarAfter);
+
+nfttabBarList.forEach((item, key) => {
+    // console.log(item);
+    item.addEventListener('click', () => {
+            // item.style.setProperty('color', 'white');
+            item.style.color = "white";
+            window.getSelection(
+                item, ':after'
+            ).backgroundColor = "red";
+
+            item.lastElementChild.style.backgroundColor = "#858584";
+            // console.log(key);
+            // console.log(item.lastChild.previousSibling);
+        // console.log(item.querySelector('.nft_tabBar-item:after'));
+        //     console.log(item.after);  //.style.setProperty('background', '#858584');
+
+            nfttabBarList.forEach((item2, key2) => {
+                    // console.log(item);
+                    if (key !== key2) {
+                        item2.style.removeProperty('color');
+                        item2.lastElementChild.style.backgroundColor = "#3B3B3B";
+                    }
+                }
+            )
+
+
+        }
+    )
 });
