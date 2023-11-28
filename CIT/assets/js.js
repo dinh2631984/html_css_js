db22076cb86e04f2d3b8651fb0018c45e0f6bc72
Phan Quoc Dinh <dinh2631984@gmail.com>
2023-11-28 23:19:36 +0700

    28-11-2023 tabBar

--- CIT/assets/js.js
+++ CIT/assets/js.js
@@ -16,10 +16,6 @@ x.addListener(myFunction) // Attach listener function on state changes*/
 const modal = document.querySelector('.modal');
 const nftNavReponsive = document.querySelector('.nft_header .nft_nav-reponsive');
 const nftNavRes = document.querySelector('.nft_nav-res');
-
-/*console.log(nftNavReponsive);
-console.log(modal)
-console.log(nftNavRes)*/
 nftNavReponsive.addEventListener('click', () => {
     modal.classList.toggle('isShow');
     // nftNavRes.classList.add('isShowRes');
@@ -34,34 +30,20 @@ nftNavRes.addEventListener('click', (e) => e.stopImmediatePropagation());
 
 /*  For tabBar    */
 const nfttabBarList = document.querySelectorAll('.nft_tabBar-item');
-// const nfttabBarAfter = document.querySelectorAll('.nft_tabBar-item::after');
-// console.log(nfttabBarAfter);
-
 nfttabBarList.forEach((item, key) => {
-    // console.log(item);
     item.addEventListener('click', () => {
             // item.style.setProperty('color', 'white');
             item.style.color = "white";
-            window.getSelection(
-                item, ':after'
-            ).backgroundColor = "red";
-
-            item.lastElementChild.style.backgroundColor = "#858584";
-            // console.log(key);
-            // console.log(item.lastChild.previousSibling);
-        // console.log(item.querySelector('.nft_tabBar-item:after'));
-        //     console.log(item.after);  //.style.setProperty('background', '#858584');
+            item.style.borderBottom = '.2rem solid #858584';
 
             nfttabBarList.forEach((item2, key2) => {
-                    // console.log(item);
                     if (key !== key2) {
                         item2.style.removeProperty('color');
-                        item2.lastElementChild.style.backgroundColor = "#3B3B3B";
+                        item2.style.removeProperty('border-bottom');
+                        // item2.lastElementChild.style.backgroundColor = "#3B3B3B";
                     }
                 }
             )
-
-
         }
     )
 });
