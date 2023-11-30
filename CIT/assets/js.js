2820046c96f13b2c7e7d4d0838714a87a215f31c
dinh2631984 <dinh2631984@gmail.com>
2023-11-30 13:50:30 +0700

    30-11-2023 NFT MarketingPlate OK

--- CIT/assets/js.js
+++ CIT/assets/js.js
@@ -33,13 +33,18 @@ const nfttabBarList = document.querySelectorAll('.nft_tabBar-item');
 nfttabBarList.forEach((item, key) => {
     item.addEventListener('click', () => {
             // item.style.setProperty('color', 'white');
+            /*
             item.style.color = "white";
             item.style.borderBottom = '.2rem solid #858584';
+            */
+            item.classList.add('nft_tabBar-active');
 
             nfttabBarList.forEach((item2, key2) => {
                     if (key !== key2) {
-                        item2.style.removeProperty('color');
-                        item2.style.removeProperty('border-bottom');
+                        /* item2.removeAttribute('style');*/
+                        item2.classList.remove('nft_tabBar-active');
+                        // item2.style.removeProperty('color');
+                        // item2.style.removeProperty('border-bottom');
                         // item2.lastElementChild.style.backgroundColor = "#3B3B3B";
                     }
                 }
